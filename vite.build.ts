6320a9e179c37c88010e35d29c3fd4bc6f4e7cc2
loeify <loeify@gmail.com>
2023-05-08 23:57:16 +0800

    feat: build

---
+++ vite.build.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: './dist',
+    rollupOptions: {
+      external: ["react", "react-router-dom"],
+    },
+    minify: false,
+    lib: {
+      entry: './src/use-query/index.ts',
+      formats: ['cjs', 'es'],
+      fileName: 'index',
+    },
+  },
+})
